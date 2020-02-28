32d2064b01ec5d2f4150ab7a053ed7f383472b2e
Ann Shumylo <annie.shumylo@gmail.com>
2020-02-28 02:32:19 +0200

    Implement parseSandwichJson
    Change-Id: I6b2230d122d6c82a7d365be1c6426cc53930cb7d

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,12 +3,16 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -21,6 +25,10 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+        TextView placeOfOrigin = findViewById(R.id.origin_tv);
+        TextView alsoKnown = findViewById(R.id.also_known_tv);
+        TextView description = findViewById(R.id.description_tv);
+        TextView ingredients = findViewById(R.id.ingredients_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -46,9 +54,21 @@ public class DetailActivity extends AppCompatActivity {
         populateUI();
         Picasso.with(this)
                 .load(sandwich.getImage())
+                .placeholder(R.drawable.no_image_placeholder)
                 .into(ingredientsIv);
 
         setTitle(sandwich.getMainName());
+
+        placeOfOrigin.setText(sandwich.getPlaceOfOrigin());
+        alsoKnown.setText(sandwich.getAlsoKnownAs().toString());
+        description.setText(sandwich.getDescription());
+
+        List<String> ingredientsList = sandwich.getIngredients();
+        ingredients.setText(TextUtils.join(", ", ingredientsList));
+
+        List<String> knownAsList = sandwich.getAlsoKnownAs();
+        alsoKnown.setText(TextUtils.join(", ", knownAsList));
+
     }
 
     private void closeOnError() {
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,51 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
+    private static final String PRODUCT_BY_NAME = "name";
+    private static final String MAIN_NAME = "mainName";
+    private static final String ALSO_KNOWN_AS = "alsoKnownAs";
+    private static final String PLACE_OF_ORIGIN = "placeOfOrigin";
+    private static final String DESCRIPTION = "description";
+    private static final String IMAGE = "image";
+    private static final String INGREDIENTS = "ingredients";
 
     public static Sandwich parseSandwichJson(String json) {
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+
+            JSONObject name = jsonObject.getJSONObject(PRODUCT_BY_NAME);
+            String mainName = name.getString(MAIN_NAME);
+
+            JSONArray knownAdArray = name.getJSONArray(ALSO_KNOWN_AS);
+            List<String> knownAsList = getListFromJsonArray(knownAdArray);
+
+            String placeOfOrigin = jsonObject.getString(PLACE_OF_ORIGIN);
+            String description = jsonObject.getString(DESCRIPTION);
+            String image = jsonObject.getString(IMAGE);
+
+            JSONArray ingredientsArray = jsonObject.getJSONArray(INGREDIENTS);
+            List<String> ingredientsList = getListFromJsonArray(ingredientsArray);
+
+            return new Sandwich(mainName, knownAsList, placeOfOrigin, description, image, ingredientsList);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         return null;
+    }
+
+    private static List<String> getListFromJsonArray(JSONArray jsonArray) throws JSONException {
+        List<String> ingredientsList = new ArrayList<>();
+        for (int i = 0; i < jsonArray.length(); i++) {
+            ingredientsList.add(jsonArray.getString(i));
+        }
+        return ingredientsList;
     }
 }
