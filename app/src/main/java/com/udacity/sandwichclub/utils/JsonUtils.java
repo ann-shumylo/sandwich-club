3b27cbfd91edf9cf69c0510c60c3ec4f9393cd26
Ann Shumylo <annie.shumylo@gmail.com>
2020-02-29 17:14:17 +0200

    Use optString instead of getString
    Change-Id: I8fb0118bacba1d8a4e7e194dd2000ccaa5900861

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -23,14 +23,14 @@ public class JsonUtils {
             JSONObject jsonObject = new JSONObject(json);
 
             JSONObject name = jsonObject.getJSONObject(PRODUCT_BY_NAME);
-            String mainName = name.getString(MAIN_NAME);
+            String mainName = name.optString(MAIN_NAME);
 
             JSONArray knownAdArray = name.getJSONArray(ALSO_KNOWN_AS);
             List<String> knownAsList = getListFromJsonArray(knownAdArray);
 
-            String placeOfOrigin = jsonObject.getString(PLACE_OF_ORIGIN);
-            String description = jsonObject.getString(DESCRIPTION);
-            String image = jsonObject.getString(IMAGE);
+            String placeOfOrigin = jsonObject.optString(PLACE_OF_ORIGIN);
+            String description = jsonObject.optString(DESCRIPTION);
+            String image = jsonObject.optString(IMAGE);
 
             JSONArray ingredientsArray = jsonObject.getJSONArray(INGREDIENTS);
             List<String> ingredientsList = getListFromJsonArray(ingredientsArray);
@@ -42,10 +42,10 @@ public class JsonUtils {
         return null;
     }
 
-    private static List<String> getListFromJsonArray(JSONArray jsonArray) throws JSONException {
+    private static List<String> getListFromJsonArray(JSONArray jsonArray) {
         List<String> ingredientsList = new ArrayList<>();
         for (int i = 0; i < jsonArray.length(); i++) {
-            ingredientsList.add(jsonArray.getString(i));
+            ingredientsList.add(jsonArray.optString(i));
         }
         return ingredientsList;
     }
